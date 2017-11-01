8a27a444833bd781db274f6d40d991f631709df8
berwin <berwin1995@qq.com>
2017-11-01 11:50:33 +0800

    Compatible with cli2.1

--- metadata.js
+++ metadata.js
@@ -18,53 +18,53 @@ module.exports = {
     "src/**/*.vue",
     "src/assets/*.png"
   ],
-  "filesignore": [
-    "src/controller/index.tpl.js",
-    "src/controller/restIndex.tpl.js",
-    "src/controller/restIndex.js",
-    "src/controller/rest.js",
-    "src/adapter",
-    "src/middleware",
-    "src/service"
-  ],
-  "multiModule": [
+  "new": [
     ["src/bootstrap", "src/common/bootstrap"],
     ["src/config", "src/common/config"],
-    ["src/config/config.js", "src/[defaultModule]/config/config.js"],
-    ["src/controller", "src/[defaultModule]/controller"],
-    ["src/logic", "src/[defaultModule]/logic"],
-    ["src/model", "src/[defaultModule]/model"],
-    ["view/index_index.html", "view/[defaultModule]/index_index.html"]
+    ["src/config/config.js", "src/[moduleName]/config/config.js"],
+    ["src/controller/base.js", "src/[moduleName]/controller/base.js"],
+    ["src/controller/index.js", "src/[moduleName]/controller/index.js"],
+    ["src/logic", "src/[moduleName]/logic"],
+    ["src/model", "src/[moduleName]/model"],
+    ["view/index_index.html", "view/[moduleName]/index_index.html"],
+    ["development.js", "development.js"],
+    ["eslintrc", ".eslintrc"],
+    ["gitignore", ".gitignore"],
+    ["nginx.conf", "nginx.conf"],
+    ["package.json", "package.json"],
+    ["pm2.json", "pm2.json"],
+    ["production.js", "production.js"],
+    ["README.md", "README.md"]
   ],
   "controller": {
     "default": [
-      ["src/controller/index.tpl.js", "/controller/[name].js"],
-      ["src/logic/index.js", "/logic/[name].js"]
+      ["src/controller/index.tpl.js", "src/[moduleName]/controller/[name].js"],
+      ["src/logic/index.js", "src/[moduleName]/logic/[name].js"]
     ],
     "rest": [
-      ["src/controller/rest.js", "controller/rest.js"],
-      ["src/controller/restIndex.tpl.js", "/controller/[name].js"],
-      ["src/logic/index.js", "/logic/[name].js"]
+      ["src/controller/rest.js", "src/[moduleName]/controller/rest.js"],
+      ["src/controller/restIndex.tpl.js", "src/[moduleName]/controller/[name].js"],
+      ["src/logic/index.js", "src/[moduleName]/logic/[name].js"]
     ]
   },
   "model": [
-    ["src/model/index.js", "/model/[name].js"]
+    ["src/model/index.js", "src/[moduleName]/model/[name].js"]
   ],
   "service": [
-    ["src/service/index.js", "/service/[name].js"]
+    ["src/service/index.js", "src/[moduleName]/service/[name].js"]
   ],
   "middleware": [
-    ["src/middleware/base.js", "/middleware/[name].js"]
+    ["src/middleware/base.js", "src/[moduleName]/middleware/[name].js"]
   ],
   "adapter": [
-    ["src/adapter/base.js", "/adapter/[type]/[name].js"]
+    ["src/adapter/base.js", "src/[moduleName]/adapter/[type]/[name].js"]
   ],
   "module": [
-    ["src/config/config.js", "/config/config.js"],
-    ["src/controller/base.js", "/controller/base.js"],
-    ["src/controller/index.js", "/controller/index.js"],
-    ["src/logic/index.js", "/logic/index.js"],
-    ["src/model/index.js", "/model/index.js"],
-    ["view/index_index.html", "/index_index.html", "view"]
+    ["src/config/config.js", "src/[moduleName]/config/config.js"],
+    ["src/controller/base.js", "src/[moduleName]/controller/base.js"],
+    ["src/controller/index.js", "src/[moduleName]/controller/index.js"],
+    ["src/logic/index.js", "src/[moduleName]/logic/index.js"],
+    ["src/model/index.js", "src/[moduleName]/model/index.js"],
+    ["view/index_index.html", "view/[moduleName]/index_index.html"]
   ]
 }
