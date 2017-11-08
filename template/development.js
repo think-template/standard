a8f82764e3c5e44593399ffc316192c4999e5469
berwin <berwin1995@qq.com>
2017-11-08 11:48:53 +0800

    merge development.babel.js and development.raw.js to development.js

--- template/development.js
+++ template/development.js
@@ -1,5 +1,16 @@
-<% if (babel) {
-  %><% include development.babel.js %><% 
-} else {
-  %><% include development.raw.js %><%
-} %>
+const Application = require('thinkjs');
+const watcher = require('think-watcher');<% if (babel) { %>
+const babel = require('think-babel');
+const notifier = require('node-notifier');<% } %>
+
+const instance = new Application({
+  ROOT_PATH: __dirname,
+  watcher: watcher,<% if (babel) { %>
+  transpiler: [babel, {
+    presets: ['think-node']
+  }],
+  notifier: notifier.notify.bind(notifier),<% } %>
+  env: 'development'
+});
+
+instance.run();
